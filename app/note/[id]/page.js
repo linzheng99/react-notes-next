d12a70c10546e54a087bc879fa1455bf20ccb34e
linzheng <alidlg9045@126.com>
2024-03-30 16:46:59 +0800

    笔记编辑 保存 和 删除

--- app/note/[id]/page.js
+++ app/note/[id]/page.js
@@ -6,10 +6,6 @@ export default async function Page({ params }) {
   const noteId = params.id;
   const note = await getNote(noteId)
 
-  // 为了让 Suspense 的效果更明显
-  const sleep = ms => new Promise(r => setTimeout(r, ms));
-  await sleep(5000);
-
   if (note == null) {
     return (
       <div className="note--empty-state">
