b9fc2774b77d856c3e753067d1d157114125db44
linzheng <alidlg9045@126.com>
2024-03-31 21:17:03 +0800

    fix: hook in condition

--- app/i18n/client.js
+++ app/i18n/client.js
@@ -35,20 +35,19 @@ export function useTranslation(lng, ns, options) {
   const { i18n } = ret
   if (runsOnServerSide && lng && i18n.resolvedLanguage !== lng) {
     i18n.changeLanguage(lng)
-  } else {
-    const [activeLng, setActiveLng] = useState(i18n.resolvedLanguage)
-    useEffect(() => {
-      if (activeLng === i18n.resolvedLanguage) return
-      setActiveLng(i18n.resolvedLanguage)
-    }, [activeLng, i18n.resolvedLanguage])
-    useEffect(() => {
-      if (!lng || i18n.resolvedLanguage === lng) return
-      i18n.changeLanguage(lng)
-    }, [lng, i18n])
-    useEffect(() => {
-      if (cookies.i18next === lng) return
-      setCookie(cookieName, lng, { path: '/' })
-    }, [lng, cookies.i18next])
   }
+  const [activeLng, setActiveLng] = useState(i18n.resolvedLanguage)
+  useEffect(() => {
+    if (activeLng === i18n.resolvedLanguage) return
+    setActiveLng(i18n.resolvedLanguage)
+  }, [activeLng, i18n.resolvedLanguage])
+  useEffect(() => {
+    if (!lng || i18n.resolvedLanguage === lng) return
+    i18n.changeLanguage(lng)
+  }, [lng, i18n])
+  useEffect(() => {
+    if (cookies.i18next === lng) return
+    setCookie(cookieName, lng, { path: '/' })
+  }, [lng, cookies.i18next])
   return ret
 }
